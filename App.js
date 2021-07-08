4bdd2907e8217f4508cae98eff4bec2aedbca9fc
John Trujillo <amriikk@gmail.com>
2021-07-08 02:22:21 -0700

    create md

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>ergo.Phile</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App() extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>ergo.Phile</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
