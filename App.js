ce2ac19686d33d478ee9c7ad2961729dc3563c55
John Trujillo <amriikk@gmail.com>
2021-07-08 02:56:12 -0700

    add zelfie + css fo rimg

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
+import { AppRegistry, StyleSheet, Text, View, Image } from 'react-native';
+import zelfie from './img/zelfie.png'
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Text, View } from 'react-native';
 
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>ergo.Phile</Text>
+        
+        <Image source={ zelfie } style={styles.image}></Image>
         <StatusBar style="auto" />
       </View>
     );
@@ -16,10 +18,15 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    width: 300,
+    height: 460,
+    borderRadius: 16,
+  }
 });
 
 AppRegistry.registerComponent('App', () => App);
