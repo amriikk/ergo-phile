591bc6d451a8859fa9f4a43f21e343bcaefac7b0
John Trujillo <amriikk@gmail.com>
2021-07-08 03:21:13 -0700

    update styles sheet

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { AppRegistry, StyleSheet, Text, View, Image } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
 import zelfie from './img/zelfie.png'
 import React, { Component } from 'react';
 
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        
-        <Image source={ zelfie } style={styles.image}></Image>
-        <StatusBar style="auto" />
-      </View>
+      <ImageBackground style={ styles.headerBackground } source={require('./img/sharingan.jpeg')}>
+        <View style={ styles.header }>
+          <View style={ styles.profilePicWrap }>
+            <Image style= { styles.profilePic } source={require('./img/zelfie.png')} />
+          </View>
+          <Text style={styles.name}>JHON TRUJILLO</Text>
+          <Text style={styles.pos}> - SOFTWARE ENGINEER - </Text>
+        </View>
+      </ImageBackground>
     );
   }
 }
@@ -22,11 +26,35 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  image: {
+  headerBackground: {
+    flex: 1,
+    width: null,
+    alignSelf: 'stretch',
+  },
+  header: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+    backgroundColor: 'rgba(0,0,0, 0.5)'
+  },
+  profilePicWrap: {
+    width: 180,
+    height: 180,
+    borderRadius: 100,
+    borderColor:'rgba(0,0,0,0.4)'
+  },
+  profilePic: {
     width: 300,
     height: 460,
     borderRadius: 16,
-  }
+  },
+  name: {
+
+  },
+  pos: {
+
+  },
 });
 
 AppRegistry.registerComponent('App', () => App);
